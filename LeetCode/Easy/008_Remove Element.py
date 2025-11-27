9a31b3dc3889111234c7ae8a5305a9acff832707
Yudhy Prayitno <yudhy.prayitno@legit-group.id>
2025-11-27 17:25:05 +0800

    Day-11

---
+++ LeetCode/Easy/008_Remove Element.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        t = 0
+        for i in range(len(nums)):
+            if nums[i]!=val:
+                nums[t]=nums[i]
+                t+=1
+        return t
